3cdc51703e09afba7fa4ece5ecc895936a778f62
Temmy <thanh.nguyenqa@snaptec.co>
2022-04-24 17:28:12 +0700

    new file

--- public/tutorial.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-function Hello(props) {
-  return <h1>Hello World!</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Hello />)  
---
+++ src/firstTutorial.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+function Hello(props) {
+    return <h1>Hello World!</h1>;
+  }
+  
+  render(
+      <Hello></Hello>
+  )
